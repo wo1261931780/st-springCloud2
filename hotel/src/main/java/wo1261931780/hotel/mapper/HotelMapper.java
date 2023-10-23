bab57c71b569d88683e755114e7ac63cd9fb595e
junw <wo1261931780@gmail.com>
2023-10-23 22:27:05 +0800

    1

--- hotel/src/main/java/wo1261931780/hotel/mapper/HotelMapper.java
+++ hotel/src/main/java/wo1261931780/hotel/mapper/HotelMapper.java
@@ -4,6 +4,9 @@ package wo1261931780.hotel.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import wo1261931780.hotel.pojo.Hotel;
+/**
+ * @author 64234
+ */
 @Mapper
 public interface HotelMapper extends BaseMapper<Hotel> {
 }
