154811cc8e79f4344bee5d9e585dcaf3077e4b22
junw <wo1261931780@gmail.com>
2024-03-04 22:47:54 +0800

    完成day1

--- src/main/java/wo1261931780/stspringCloud2/pojo/HotelDoc.java
+++ src/main/java/wo1261931780/stspringCloud2/pojo/HotelDoc.java
@@ -16,7 +16,7 @@ public class HotelDoc {
     private String city;
     private String starName;
     private String business;
-    private String location;
+    private String location;// 大部分的字段都是一样的，经纬度不一样
     private String pic;
 
     public HotelDoc(Hotel hotel) {
@@ -30,6 +30,8 @@ public class HotelDoc {
         this.starName = hotel.getStarName();
         this.business = hotel.getBusiness();
         // 这里是为了匹配location字段的格式
+        // 其实是可以直接使用BeanUtil来完成这个操作，
+        // 主要的目的是将经纬度放到一起。实现数据库数据到索引库数据的转换
         this.location = hotel.getLatitude() + ", " + hotel.getLongitude();
         this.pic = hotel.getPic();
     }
--- src/test/java/wo1261931780/stspringCloud2/HotelDocumentTest.java
+++ src/test/java/wo1261931780/stspringCloud2/HotelDocumentTest.java
@@ -4,7 +4,12 @@ package wo1261931780.stspringCloud2;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -19,6 +24,7 @@ import wo1261931780.stspringCloud2.pojo.HotelDoc;
 import wo1261931780.stspringCloud2.service.IHotelService;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Intellij IDEA.
@@ -78,6 +84,91 @@ public class HotelDocumentTest {
 		// 3. 关闭客户端
 	}
 
+	/**
+	 * 获取文档，实际就是查询
+	 *
+	 * @throws IOException 异常
+	 */
+	@Test
+	void testGetDocument() throws IOException {
+		GetRequest getRequest = new GetRequest("hotel", "61083");// 1. 索引库名，文档id
+		// 上面就是查询条件
+		// 因为文档中的数据就是Source中的数据，所以需要一步骤来获取
+		GetResponse documentFields = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);// 2. 调用方法，获取文档
+		log.info("文档内容：{}", documentFields.getSourceAsString());// 3. 获取文档内容
+		// 文档中的内容是json格式的
+		String sourceAsString = documentFields.getSourceAsString();// 4. 将文档内容转换成实体类对象
+		// 因为是json格式的
+		HotelDoc hotelDoc = JSON.parseObject(sourceAsString, HotelDoc.class);// 5. 反序列化
+		log.info("反序列化后的对象：{}", hotelDoc);
+	}
+
+	/**
+	 * 更新文档
+	 *
+	 * @throws IOException 异常
+	 */
+	@Test
+	void testUpdateDocument() throws IOException {
+		// 1. 创建文档对象
+		UpdateRequest hotel = new UpdateRequest("hotel", "61083");
+		// 相当于这里创建了一个查询语句来获取对象
+		// 然后再进行更新
+		hotel.doc(
+				"name", "北京饭店",
+				"starName", "四钻"
+				// 这里其实要注意，可变参数，中间是逗号
+		);// 准备请求的参数
+		// 前面是字段，后面是值
+		// 2. 调用方法，添加文档
+		restHighLevelClient.update(hotel, RequestOptions.DEFAULT);
+		// 3. 关闭客户端
+	}
+
+	/**
+	 * 删除文档
+	 *
+	 * @throws IOException 异常
+	 */
+	@Test
+	void testDeleteDocument() throws IOException {
+		// 1. 创建文档对象
+		DeleteRequest hotel = new DeleteRequest("hotel", "61083");
+
+		// 2. 调用方法，添加文档
+		restHighLevelClient.delete(hotel, RequestOptions.DEFAULT);
+		// 3. 关闭客户端
+	}
+
+	/**
+	 * 批量新增
+	 *
+	 * @throws IOException 异常
+	 */
+	@Test
+	void testBulkRequest() throws IOException {
+		List<Hotel> hotelList = hotelService.list();// 获得所有的酒店数据
+		BulkRequest bulkRequest = new BulkRequest();// 1. 创建批量请求对象
+		// 在括号当中，可以批量新增/修改/删除
+		for (Hotel hotel : hotelList) {
+			HotelDoc hotelDoc = new HotelDoc(hotel);
+			// 2. 准备请求，添加多个新增的Request
+			bulkRequest.add(new IndexRequest("hotel")
+					.id(hotel.getId().toString())
+					.source(JSON.toJSONString(hotelDoc), XContentType.JSON));
+			// IndexRequest request = new IndexRequest("hotel").id(hotel.getId().toString());
+			// request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
+			// restHighLevelClient.index(request, RequestOptions.DEFAULT);
+		}
+		// 2. 准备请求，添加多个新增的Request
+		// bulkRequest.add(new IndexRequest("hotel").id("1").source(XContentType.JSON, "name", "北京饭店"));
+		// 新增的好几个，就是多个IndexRequest
+		// bulkRequest.add(new IndexRequest("hotel").id("2").source(XContentType.JSON, "name", "北京饭店"));
+		// bulkRequest.add(new IndexRequest("hotel").id("3").source(XContentType.JSON, "name", "北京饭店"));
+		// 因为有了一个转化为文档对象的循环过程
+		// 所以，这里就不需要再写多个IndexRequest了
+		restHighLevelClient.bulk(null, RequestOptions.DEFAULT);
+	}
 	// @Test
 	// void testMatchAll() throws IOException {
 	// 	// 1.准备request
@@ -212,58 +303,5 @@ public class HotelDocumentTest {
 	// // }
 	//
 	//
-	// @Test
-	// void testGetDocument() throws IOException {
-	// 	GetRequest getRequest = new GetRequest("hotel", "61083");// 1. 索引库名，文档id
-	// 	GetResponse documentFields = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);// 2. 调用方法，获取文档
-	// 	log.info("文档内容：{}", documentFields.getSourceAsString());// 3. 获取文档内容
-	// 	String sourceAsString = documentFields.getSourceAsString();// 4. 将文档内容转换成实体类对象
-	// 	HotelDoc hotelDoc = JSON.parseObject(sourceAsString, HotelDoc.class);// 5. 反序列化
-	// 	log.info("反序列化后的对象：{}", hotelDoc);
-	// }
-	//
-	// @Test
-	// void testUpdateDocument() throws IOException {
-	// 	// 1. 创建文档对象
-	// 	UpdateRequest hotel = new UpdateRequest("hotel", "61083");
-	// 	hotel.doc("name", "北京饭店");// 准备请求的参数
-	//
-	// 	// 2. 调用方法，添加文档
-	// 	restHighLevelClient.update(hotel, RequestOptions.DEFAULT);
-	// 	// 3. 关闭客户端
-	// }
-	//
-	// @Test
-	// void testDeleteDocument() throws IOException {
-	// 	// 1. 创建文档对象
-	// 	DeleteRequest hotel = new DeleteRequest("hotel", "61083");
-	//
-	// 	// 2. 调用方法，添加文档
-	// 	restHighLevelClient.delete(hotel, RequestOptions.DEFAULT);
-	// 	// 3. 关闭客户端
-	// }
-	//
-	// @Test
-	// void testBulkRequest() throws IOException {
-	// 	List<Hotel> hotelList = hotelService.list();// 获得所有的酒店数据
-	// 	BulkRequest bulkRequest = new BulkRequest();// 1. 创建批量请求对象
-	// 	for (Hotel hotel : hotelList) {
-	// 		HotelDoc hotelDoc = new HotelDoc(hotel);
-	// 		// 2. 准备请求，添加多个新增的Request
-	// 		bulkRequest.add(new IndexRequest("hotel")
-	// 				.id(hotel.getId().toString())
-	// 				.source(JSON.toJSONString(hotelDoc), XContentType.JSON));
-	// 		// IndexRequest request = new IndexRequest("hotel").id(hotel.getId().toString());
-	// 		// request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
-	// 		// restHighLevelClient.index(request, RequestOptions.DEFAULT);
-	// 	}
-	// 	// 2. 准备请求，添加多个新增的Request
-	// 	// bulkRequest.add(new IndexRequest("hotel").id("1").source(XContentType.JSON, "name", "北京饭店"));
-	// 	// 新增的好几个，就是多个IndexRequest
-	// 	// bulkRequest.add(new IndexRequest("hotel").id("2").source(XContentType.JSON, "name", "北京饭店"));
-	// 	// bulkRequest.add(new IndexRequest("hotel").id("3").source(XContentType.JSON, "name", "北京饭店"));
-	// 	// 因为有了一个转化为文档对象的循环过程
-	// 	// 所以，这里就不需要再写多个IndexRequest了
-	// 	restHighLevelClient.bulk(null, RequestOptions.DEFAULT);
-	// }
+
 }
