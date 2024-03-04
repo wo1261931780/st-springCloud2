a78a14636e212d752053bbfaf70e79a4f7d74d16
junw <wo1261931780@gmail.com>
2024-03-04 10:33:37 +0800

    1

--- src/main/java/wo1261931780/hotel/service/IHotelService.java
+++
@@ -1,8 +0,0 @@
-package wo1261931780.hotel.service;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import wo1261931780.hotel.pojo.Hotel;
-
-public interface IHotelService extends IService<Hotel> {
-}
